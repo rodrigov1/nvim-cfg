b14259dca56f83ef1810a2c893b5554aaa8e1ec4
rodrigov1 <rodrigo.vargas@mi.unc.edu.ar>
2024-03-08 20:21:12 -0300

    feat: updated telescope-layout to my liking

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -1,1 +1,26 @@
-return {}
+return {
+  {
+    'telescope.nvim',
+    dependencies = {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+      config = function()
+        require('telescope').load_extension 'fzf'
+      end,
+    },
+  },
+  require('telescope').setup {
+    defaults = {
+      layout_strategy = 'vertical',
+      layout_config = {
+        vertical = {
+          width = 0.7,
+          preview_cutoff = 1,
+          preview_height = 0.45,
+          prompt_position = 'bottom',
+          scroll_speed = 3,
+        },
+      },
+    },
+  },
+}
