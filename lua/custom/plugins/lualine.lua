ce9a264fff30d94d7837729eaba90a2a79d86f40
rodrigov1 <rodrigo.vargas@mi.unc.edu.ar>
2024-03-11 17:35:39 -0300

    fix: lualine now shows the last C.C.M (count-command-motion)

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -13,12 +13,22 @@ return {
       },
       sections = {
         lualine_b = {'branch','diagnostics','diff'},
+        -- With this empty table, lualine_c (default) is disabled
         lualine_c = {},
-        lualine_x = {'filetype'},
+        lualine_x = {
+            -- This function returns the last (count - command - motion) action to display in lualine
+          {
+              function() return require("noice").api.status.command.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+              color = { fg = "#ff9e64" },
+          },
+          {'filetype'},
+        },
         lualine_y = {
           { "progress", separator = " ", padding = { left = 1, right = 1 } },
         },
         lualine_z = {
+          -- This function returns the current time
           function()
             return " " .. os.date("%R")
           end,
